b1b6ac8f1e1ade06cde6e2407e1481afff561e08
Stan <reznik333@gmail.com>
2019-10-19 21:27:03 +0300

    rewrite 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'animals/index'
   resources :animals
-  root 'welcome#index'
+  root 'animals#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
